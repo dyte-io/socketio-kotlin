cefb229e9f88d7380c30628d2c42a99751473d38
Harsh Shandilya <harsh.shandilya@dyte.io>
2023-10-16 12:21:36 +0530

    chore(build): upgrade to latest AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
   // trick: for the same plugin versions in all sub-modules
-  id("com.android.library").version("7.3.1").apply(false)
+  id("com.android.library").version("8.1.2").apply(false)
   kotlin("multiplatform").version("1.9.0").apply(false)
   id("io.dyte.gradle.plugin.base") version "0.0.4"
 }
