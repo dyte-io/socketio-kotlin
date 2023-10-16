76911e564b10bccaada5afddd36df5ba7c4167bc
Harsh Shandilya <harsh.shandilya@dyte.io>
2023-10-16 13:00:01 +0530

    refactor: cleanup `EventEmitter`

--- socketio/src/commonMain/kotlin/io/dyte/socketio/EventEmitter.kt
+++ socketio/src/commonMain/kotlin/io/dyte/socketio/EventEmitter.kt
@@ -3,39 +3,39 @@ package io.dyte.socketio
 /** Generic event emitting and handling. */
 open class EventEmitter {
   /** Mapping of events to a list of event handlers */
-  val _events = mutableMapOf<String, MutableList<(data: Any?) -> Unit>>()
+  private val events = mutableMapOf<String, MutableList<(data: Any?) -> Unit>>()
 
   /** Mapping of events to a list of one-time event handlers */
-  val _eventsOnce = mutableMapOf<String, MutableList<(data: Any?) -> Unit>>()
+  private val eventsOnce = mutableMapOf<String, MutableList<(data: Any?) -> Unit>>()
 
   /** List of handlers that listen every event */
-  val _eventsAny = mutableListOf<(event: String, data: Any?) -> Unit>()
+  private val eventsAny = mutableListOf<(event: String, data: Any?) -> Unit>()
 
   /**
    * This function triggers all the handlers currently listening to [event] and passes them [data].
    */
   open fun emit(event: String, data: Any? = null) {
-    val list = (this._events.getOrElse(event) { listOf() }).toList()
+    val list = (this.events.getOrElse(event) { listOf() }).toList()
     Logger.debug("${list.size} event listeners registered for $event")
 
     // todo: try to optimize this. Maybe remember the off() handlers and remove later?
     // handler might be off() inside handler; make a copy first
     list.forEach { it(data) }
 
-    this._eventsOnce.remove(event)?.forEach { it(data) }
+    this.eventsOnce.remove(event)?.forEach { it(data) }
 
-    this._eventsAny.forEach { it(event, data) }
+    this.eventsAny.forEach { it(event, data) }
   }
 
   /** This function binds the [handler] as a listener to the [event] */
   fun on(event: String, handler: (data: Any?) -> Unit) {
-    this._events.getOrPut(
+    this.events.getOrPut(
       event,
       fun(): MutableList<(data: Any?) -> Unit> {
         return mutableListOf<(data: Any?) -> Unit>()
       }
     )
-    this._events[event]?.add(handler)
+    this.events[event]?.add(handler)
   }
 
   /**
@@ -43,34 +43,34 @@ open class EventEmitter {
    * [handler] is called once, it is removed.
    */
   fun once(event: String, handler: (data: Any?) -> Unit) {
-    this._eventsOnce.getOrPut(
+    this.eventsOnce.getOrPut(
       event,
       fun(): MutableList<(data: Any?) -> Unit> {
         return mutableListOf<(data: Any?) -> Unit>()
       }
     )
-    this._eventsOnce[event]?.add(handler)
+    this.eventsOnce[event]?.add(handler)
   }
 
   /** This function binds the [handler] as a listener to any event */
   fun onAny(handler: (event: String, data: Any?) -> Unit) {
-    this._eventsAny.add(handler)
+    this.eventsAny.add(handler)
   }
 
   /** This function attempts to unbind the [handler] from the [event] */
   fun off(event: String, handler: ((data: Any?) -> Unit)?) {
     if (handler != null) {
-      this._events[event]?.remove(handler)
-      this._eventsOnce[event]?.remove(handler)
-      if (this._events[event]?.isEmpty() == true) {
-        this._events.remove(event)
+      this.events[event]?.remove(handler)
+      this.eventsOnce[event]?.remove(handler)
+      if (this.events[event]?.isEmpty() == true) {
+        this.events.remove(event)
       }
-      if (this._eventsOnce[event]?.isEmpty() == true) {
-        this._eventsOnce.remove(event)
+      if (this.eventsOnce[event]?.isEmpty() == true) {
+        this.eventsOnce.remove(event)
       }
     } else {
-      this._events.remove(event)
-      this._eventsOnce.remove(event)
+      this.events.remove(event)
+      this.eventsOnce.remove(event)
     }
   }
 
@@ -78,24 +78,23 @@ open class EventEmitter {
    * This function attempts to unbind the [handler]. If the given [handler] is null, this function
    * unbinds all any event handlers.
    */
-  fun offAny(handler: (event: String, data: Any?) -> Unit) {
+  fun offAny(handler: ((event: String, data: Any?) -> Unit)?) {
     if (handler != null) {
-      this._eventsAny.remove(handler)
+      this.eventsAny.remove(handler)
     } else {
-      this._eventsAny.clear()
+      this.eventsAny.clear()
     }
   }
 
   /** This function unbinds all the handlers for all the events. */
   fun clearListeners() {
-    this._events.clear()
-    this._eventsOnce.clear()
-    this._eventsAny.clear()
+    this.events.clear()
+    this.eventsOnce.clear()
+    this.eventsAny.clear()
   }
 
   /** Returns whether the event has registered. */
   fun hasListeners(event: String): Boolean {
-    return this._events[event]?.isNotEmpty() == true ||
-      this._eventsOnce[event]?.isNotEmpty() == true
+    return this.events[event]?.isNotEmpty() == true || this.eventsOnce[event]?.isNotEmpty() == true
   }
 }
